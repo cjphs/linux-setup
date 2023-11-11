08d946c5096c69869f083d3d0cc241633ef98d90
orb <orb@Akagi.home>
2023-11-11 03:01:56 +0100

    vscode optional

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ bash ./setup-folders.sh
 bash ./setup-packages.sh
 bash ./setup-resources.sh
 
-# only do this if --vscode is present in the arguments
 if [[ "$*" == *--vscode* ]]; then
     echo "Setting up VSCode..."
     bash ./setup-vscode.sh
