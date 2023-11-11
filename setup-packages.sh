ad108cd203c6d73758d9f6243ca6f6b947456a18
orb <orb@Akagi.home>
2023-11-11 03:02:55 +0100

    ordering

--- setup-packages.sh
+++ setup-packages.sh
@@ -14,10 +14,11 @@ sudo apt install default-jdk -y
 sudo apt install openjdk-17-jdk -y
 
 # Python
-sudo apt install python3 -y
 sudo apt install pip -y
+sudo apt install python3 -y
 
 # Media
+sudo apt install feh -y
 sudo apt install vlc -y
 # https://github.com/yt-dlp/yt-dlp
 sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
@@ -28,14 +29,13 @@ sudo apt install gimp -y
 sudo apt install kdenlive -y
 
 # Environment
-sudo apt install plasma-workspace-wayland -y
 sudo apt install dmenu -y
+sudo apt install plasma-workspace-wayland -y
 sudo apt install yakuake -y
 
 # Misc
-sudo apt install feh -y
-sudo apt install neofetch -y
 sudo apt install lolcat -y
+sudo apt install neofetch -y
 
 sudo apt upgrade -y
 sudo apt autoremove -y
