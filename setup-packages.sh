02db19ab29270960ab5c681d9591dc7604ca7c6d
orb <orb@Akagi.home>
2023-11-12 19:56:45 +0100

    cleanup

--- setup-packages.sh
+++ setup-packages.sh
@@ -5,65 +5,85 @@ source ./setup-setup.sh
 
 sudo apt update && sudo apt full-upgrade -y
 
-# Development
-echoMsg "Installing development tools"
-sudo apt install git -y
-sudo apt install neovim -y
-sudo apt install nodejs -y
-sudo apt install npm -y
-sudo apt install gh -y
-
-# Java
-echoMsg "Installing Java"
-sudo apt install default-jre -y
-sudo apt install default-jdk -y
-sudo apt install openjdk-17-jdk -y
-
-# Python
-echoMsg "Installing Python"
-sudo apt install pip -y
-sudo apt install python3 -y
-
-# Media
-echoMsg "Installing media packages"
-sudo apt install feh -y
-sudo apt install vlc -y
-xdg-mime default vlc.desktop audio/*
-xdg-mime default vlc.desktop video/*
-# https://github.com/yt-dlp/yt-dlp
-sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
-sudo chmod a+rx /usr/local/bin/yt-dlp
-
-# Environment
-echoMsg "Installing environment packages"
-sudo apt install btop -y
-sudo apt install gdebi -y
-sudo apt install plasma-workspace-wayland -y
-sudo apt install yakuake -y
-
-# Misc
-echoMsg "Installing misc packages"
-sudo apt install lolcat -y
-sudo apt install neofetch -y
-
-if [[ "$*" == *--lite* ]]; then
+aptInstall() {
+    sudo apt install "$1" -y
+}
+upgradeAndClean() {
     sudo apt upgrade -y
     sudo apt autoremove -y
-    
-    exit 1
-else
-    # Editing
-    echoMsg "Installing image/video processing packages"
-    sudo apt install gimp -y
-    sudo apt install kdenlive -y
+}
 
+installEnvironment() {
+    echoMsg "Installing environment packages"
+    aptInstall btop
+    aptInstall gdebi
+    aptInstall plasma-workspace-wayland
+    aptInstall yakuake
+}
+installDevelopment() {
+    echoMsg "Installing development tools"
+    aptInstall git
+    aptInstall neovim
+    aptInstall gh
+}
+installNode() {
+    echoMsg "Installing Node"
+    aptInstall nodejs
+    aptInstall npm
+}
+installJava() {
+    echoMsg "Installing Java"
+    aptInstall default-jre
+    aptInstall default-jdk
+    aptInstall openjdk-17-jdk
+}
+installPython() {
+    # Python
+    echoMsg "Installing Python"
+    aptInstall pip
+    aptInstall python3
+    alias python="python3"
+}
+installMedia() {
+    echoMsg "Installing media packages"
+    aptInstall feh
+    aptInstall vlc
+    xdg-mime default vlc.desktop audio/*
+    xdg-mime default vlc.desktop video/*
+    # https://github.com/yt-dlp/yt-dlp
+    sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+    sudo chmod a+rx /usr/local/bin/yt-dlp
+}
+installMisc() {
+    echoMsg "Installing misc packages"
+    aptInstall lolcat
+    aptInstall neofetch
+}
+installEditingTools() {
+    echoMsg "Installing image/video processing packages"
+    aptInstall gimp
+    aptInstall kdenlive
+}
+installFun() {
     echoMsg "Installing Steam and Discord"
     mkdir -p tmp
     sudo curl -L "https://cdn.akamai.steamstatic.com/client/installer/steam.deb" -o tmp/steam.deb && sudo gdebi tmp/steam.deb -n
     sudo curl -L "https://discord.com/api/download?platform=linux&format=deb" -o tmp/discord.deb && sudo gdebi tmp/discord.deb -n
     sudo rm tmp/discord.deb tmp/steam.deb
     sudo rm -rf tmp
+}
 
-    sudo apt upgrade -y
-    sudo apt autoremove -y
+installEnvironment
+installDevelopment
+installNode
+installJava
+installPython
+installMedia
+installMisc
+
+if [[ "$*" != *--lite* ]]; then
+    installEditingTools
+    installFun
 fi
+
+upgradeAndClean
